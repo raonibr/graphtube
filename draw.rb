788deb002db3385aa2234dbae07342644166aee1
raonidonordeste <raonidonordeste@af644c28-b947-11de-88a7-9da883da0866>
2009-12-30 04:31:00 +0000

    Adicionei um hash na função de desenhar arestas... Agora as arestas saem ,mas as posições ainda estão aleatórias...

--- draw.rb
+++ draw.rb
@@ -18,6 +18,8 @@ Aresta_draw: [POSICAO_DO_ELEM_1,POSICAO_DO_ELEM_2]
 class DrawView
 
 def initialize(vertices, arestas)
+  
+    # PRIMEIRO, SE DEFINEM AS GLOBAIS DESSA JANELA.
     @vertices = vertices
     @arestas = arestas
     
@@ -26,62 +28,54 @@ def initialize(vertices, arestas)
     
     @tam_vertice = 12
     
-    @Vertices_draw = []
-    @Arestas_draw = []
-
+    @Vertices_draw = Hash[]
+    @Arestas_draw = @arestas
+  
+    #Primeiro, todos os vertices são tratados e colocados no Hash Vertices_draw
     @vertices.each do |ver|
-      
       if  (ver[0]=="ID")
-        @Vertices_draw  << [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver[1],"RED"]
+        @Vertices_draw[ver[1]] = [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver[1],"RED"]
       else
-        @Vertices_draw  << [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver,"BLUE"]
+        @Vertices_draw[ver] = [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver,"BLUE"]
       end
-      
     end
     
-=begin ESSE BAGULHO NÃO FUNCIONA.... PROCURAR SOLUCAO MELHOR
-    @arestas.each do |are|
-      i = 0
-      @temp_are = []
-      while ((i < @Vertices_draw.length()) and (@temp_are.length()<2) )
-        pp @Vertices_draw[i][2]
-        if (@Vertices_draw[i][2]==are[0])
-          @temp_are << i
-          pp "RÁ\n"
-        end
-        if (@Vertices_draw[i][2]==are[1])
-          @temp_are << i
-          pp "RÁ\n"
-        end
-        i = i+1
-      end
-      @Arestas_draw << @temp_are
-    end
-=end
     
+    #Depois a drowing area é criada
     area = Gtk::DrawingArea.new
     area.set_size_request(100,100)
     
+    
+    # AQUI É O REDRAW, DEPOIS PRECISA VIRAR UM MODULO
     area.signal_connect("expose_event") do
       
       @altura = @draw_window.size.last()
       @largura = @draw_window.size.first()
-      alloc = area.allocation
       
-      @Vertices_draw.each do |p|
-        x1 = p[0]*@largura
-        y1 = p[1]*@altura
-      area.window.draw_arc(area.style.fg_gc(area.state), true, x1, y1, @tam_vertice, @tam_vertice, 0 ,64 * 360) 
       
-      # Nesse caso tam_vertice/2 representa a correção da posição do canto da linha.. Assim ela aponta para o centro do vertice, não para o canto
-      #area.window.draw_line(area.style.fg_gc(area.state), x1+@tam_vertice/2, y1+@tam_vertice/2, x2+@tam_vertice/2,y2+@tam_vertice/2)
-   
-   end
-    
+      # ESSES DOIS LOOPs ABAIXO PRECISAM SER MODULARIZADOS... FAZER ISSO NA PRÓXIMA REVISÃO.
+      
+      # NESSE LOOP, CADA VERTICE É DESENHADO.
+        @Vertices_draw.each do |p| #Hashs tb tem funcão EACH
+          x1 = p[1][0]*@largura
+          y1 = p[1][1]*@altura
+          area.window.draw_arc(area.style.fg_gc(area.state), true, x1, y1, @tam_vertice, @tam_vertice, 0 ,64 * 360) 
+        end
+        
+        
+     # NESSE LOOP, CADA ARESTA É DESENHADA.
+         @Arestas_draw.each do |a|
+            # A LINHA ABAIXO É COMPLICADA... Mas ela pega para cada aresta e recupera as coordenadas dos vertices de cada lado e desenha um linha ligando eles.
+            # Quando se introduz o "tam_vertice/2" se faz a correção da posição do canto da linha.. Assim ela aponta para o centro do vertice, não para o canto
+            area.window.draw_line(area.style.fg_gc(area.state), @Vertices_draw[a[0]][0]*@largura+@tam_vertice/2, @Vertices_draw[a[0]][1]*@altura+@tam_vertice/2, @Vertices_draw[a[1]][0]*@largura+@tam_vertice/2,@Vertices_draw[a[1]][1]*@altura+@tam_vertice/2)
+          end
+          
+          
+          
     end
     
     
-    @draw_window = Gtk::Window.new.add(area).set_size_request(300, 200)
+    @draw_window = Gtk::Window.new.add(area).set_size_request(600, 400)
     @draw_window.title = "Draw Area"
     @draw_window.show_all
     
