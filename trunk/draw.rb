1f05fa1aa5a30f2942ab2f56106e069d9e3c92e8
raonidonordeste <raonidonordeste@af644c28-b947-11de-88a7-9da883da0866>
2009-12-30 16:37:14 +0000

    Pequenas mudanças. Mudei a cor do vertice de controle e tirei um monte de processamento do loop de redraw.

--- trunk/draw.rb
+++ trunk/draw.rb
@@ -35,51 +35,47 @@ def initialize(vertices, arestas)
     
     @Vertices_draw = Hash[]
     @Arestas_draw = @arestas
-  
+    
+    
+    red   = Gdk::Color.new(65535, 0, 0)
+    yellow = Gdk::Color.new(57000, 57000, 0)
+    blue = Gdk::Color.new(0,0,  65535)
+    black = Gdk::Color.new(0,0, 0)
+    
     #Primeiro, todos os vertices são tratados e colocados no Hash Vertices_draw
     @vertices.each do |ver|
       if  (ver[0]=="ID")
-        @Vertices_draw[ver[1]] = [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver[1],"RED"]
+        if (ver[1][0] == 49)
+          @Vertices_draw[ver[1]] = [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver[1],red]
+        else
+          @Vertices_draw[ver[1]] = [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver[1],yellow]
+        end
       else
-        @Vertices_draw[ver] = [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver,"BLUE"]
+        @Vertices_draw[ver] = [(rand(10000).to_f/10000),(rand(10000).to_f/10000),ver,blue]
       end
     end
     
-    
-    #Depois a drowing area é criada
-    #area = Gtk::DrawingArea.new
-    #area.set_size_request(100,100)
-
-    #@draw_window.add(area)
-    
     drawable = @draw_window.window
-    gc = Gdk::GC.new(drawable)
+    
 
-    red   = Gdk::Color.new(65535, 0, 0)
-    green = Gdk::Color.new(0, 65535, 0)
     colormap = Gdk::Colormap.system
     colormap.alloc_color(red,   false, true)
-    colormap.alloc_color(green, false, true)
+    colormap.alloc_color(yellow, false, true)
+    colormap.alloc_color(blue, false, true)
+    colormap.alloc_color(black, false, true)
+    gc = Gdk::GC.new(drawable)
+    gc.set_background(black)
+    layout = Pango::Layout.new(Gdk::Pango.context)
+    layout.font_description = Pango::FontDescription.new('Sans 7')
     
     
     # AQUI É O REDRAW, DEPOIS PRECISA VIRAR UM MODULO
     @draw_window.signal_connect("expose_event") do |win, evt|
-      
-    drawable = @draw_window.window
-    gc = Gdk::GC.new(drawable)
 
-    red   = Gdk::Color.new(65535, 0, 0)
-    green = Gdk::Color.new(0, 65535, 0)
-    blue = Gdk::Color.new(0,0,  65535)
-    colormap = Gdk::Colormap.system
-    colormap.alloc_color(red,   false, true)
-    colormap.alloc_color(green, false, true)
-    colormap.alloc_color(blue, false, true)
-      
       @altura = @draw_window.size.last()
       @largura = @draw_window.size.first()
       
-      
+      gc.set_foreground(black)
       # ESSES DOIS LOOPs ABAIXO PRECISAM SER MODULARIZADOS... FAZER ISSO NA PRÓXIMA REVISÃO.
       # NESSE LOOP, CADA ARESTA É DESENHADA.
       @Arestas_draw.each do |a|
@@ -88,28 +84,13 @@ def initialize(vertices, arestas)
         drawable.draw_line(gc, @Vertices_draw[a[0]][0]*@largura+@tam_vertice/2, @Vertices_draw[a[0]][1]*@altura+@tam_vertice/2, @Vertices_draw[a[1]][0]*@largura+@tam_vertice/2,@Vertices_draw[a[1]][1]*@altura+@tam_vertice/2)
       end
 
-      gc.set_background(green)
-
-      layout = Pango::Layout.new(Gdk::Pango.context)
-      layout.font_description = Pango::FontDescription.new('Sans 7')
       # NESSE LOOP, CADA VERTICE É DESENHADO.
         @Vertices_draw.each do |p| #Hashs tb tem funcão EACH
           layout.text = p[1][2]
           x1 = p[1][0]*@largura
           y1 = p[1][1]*@altura
           drawable.draw_layout(@draw_window.style.fg_gc(@draw_window.state), x1, y1-10, layout)
-          if (p[1][2][0] == 49)
-            gc.set_line_attributes(10, Gdk::GC::LINE_SOLID, Gdk::GC::CAP_ROUND, Gdk::GC::JOIN_ROUND)
-            drawable.draw_arc( gc.set_foreground(red), true, x1, y1, @tam_vertice, @tam_vertice, 0 ,64 * 360) 
-          
-          elsif (p[1][2][0] == 50)
-            gc.set_line_attributes(10, Gdk::GC::LINE_SOLID, Gdk::GC::CAP_ROUND, Gdk::GC::JOIN_ROUND)
-            drawable.draw_arc( gc.set_foreground(green), true, x1, y1, @tam_vertice, @tam_vertice, 0 ,64 * 360)
-           
-          else
-            gc.set_line_attributes(10, Gdk::GC::LINE_SOLID, Gdk::GC::CAP_ROUND, Gdk::GC::JOIN_ROUND)
-            drawable.draw_arc( gc.set_foreground(blue), true, x1, y1, @tam_vertice, @tam_vertice, 0 ,64 * 360)
-          end  
+          drawable.draw_arc( gc.set_foreground(p[1][3]), true, x1, y1, @tam_vertice, @tam_vertice, 0 ,64 * 360) 
         end
        
     end
