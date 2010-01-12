731c1b277af35039dc8db0456784304ed215479c
nailalves <nailalves@af644c28-b947-11de-88a7-9da883da0866>
2010-01-12 04:14:48 +0000

    Add buffer na viewPP.

--- trunk/viewPP.rb
+++ trunk/viewPP.rb
@@ -17,6 +17,59 @@ class ViewPP
     
     caixa_comandos = Gtk::VBox.new(true,10)
   #---------------------------------------------------------------------------------------------#
+  
+    #-----------------------------TEXTVIEW_1 RELACIONADA A PESSOAS----------------------------------#
+
+    textview_1 = Gtk::TextView.new
+
+    textview_1.buffer.text = ""
+
+    textview_1.editable = false
+
+    textview_1.cursor_visible = false
+
+    textview_1.border_width = 0
+
+
+    scrolled_textview_1 = Gtk::ScrolledWindow.new
+
+    scrolled_textview_1.border_width = 0
+    
+    scrolled_textview_1.add(textview_1)
+
+    scrolled_textview_1.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS)
+    
+  #---------------------------------------------------------------------------------------------#
+
+  #-----------------------------TEXTVIEW_2 RELACIONADA AS ARESTAS----------------------------------#
+    textview_2 = Gtk::TextView.new
+
+    textview_2.buffer.text = ""
+
+    textview_2.editable = false
+
+    textview_2.cursor_visible = false
+
+    textview_2.border_width = 0
+
+
+    scrolled_textview_2 = Gtk::ScrolledWindow.new
+
+    scrolled_textview_2.border_width = 0
+    
+    scrolled_textview_2.add(textview_2)
+
+    scrolled_textview_2.set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_ALWAYS)
+  #---------------------------------------------------------------------------------------------#
+  
+  #----------------------------------CRIA AS LABELS---------------------------------------------#
+	textviews = Gtk::Notebook.new
+	label1 = Gtk::Label.new("Pessoas")
+	label2 = Gtk::Label.new("Arestas")
+	textviews.append_page(scrolled_textview_1, label1)
+	textviews.append_page(scrolled_textview_2, label2)
+  caixa_comandos.pack_start(textviews,true,true,0)
+	#---------------------------------------------------------------------------------------------#
 
   #------------------------------CRIA O BOTÃO DE DESENHAR---------------------------------------#
     botao_desenhar = Gtk::Button.new("Desenhar GRAFO")
@@ -41,11 +94,9 @@ class ViewPP
     
     caixa_comandos.pack_start(botao_exportar,true,false,0)
   #---------------------------------------------------------------------------------------------#
-
+   
     window.add(caixa_comandos)
     
     window.show_all
-    
-    Gtk.main
   end
 end
