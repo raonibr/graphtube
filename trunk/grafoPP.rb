cdac7f02795184d1218adc69a725d27657972700
raonidonordeste@gmail.com <raonidonordeste@gmail.com@af644c28-b947-11de-88a7-9da883da0866>
2010-01-12 05:20:43 +0000

    Agora ele já gera o grafo pessoa-pessoa corretamente!

--- trunk/grafoPP.rb
+++ trunk/grafoPP.rb
@@ -27,11 +27,39 @@ class GrafoPP < Grafo
   def initialize(grafo_base)
     @vertices = []
     @arestas = []
+    # Arestas_aux será um hash que guardará os grupos de pessoas que frequentam o mesmo lugar
+    # Cada bó do hash representa um lugar e é um vetor de pessoas.. (A chave do hash é o nome do local)
+    @arestas_aux = Hash[]
     grafo_base.vertices().each do |pessoa|
       if(pessoa[0]=="ID")
         @vertices << pessoa
       end
     end
+    
+    grafo_base.arestas().each do |aresta|
+      if (@arestas_aux[aresta[1]] == nil)
+        @arestas_aux[aresta[1]] = []
+        @arestas_aux[aresta[1]] << aresta[0]
+      else
+        @arestas_aux[aresta[1]] << aresta[0]
+      end
+    end
+    
+    @arestas_aux.each do |grupo|
+      self.cria_clique(grupo[1])
+    end
+    
   end
   
+  
+  def cria_clique(grupo)
+    for i in 0 .. (grupo.length()-1)
+      for j in i .. (grupo.length()-1)
+        if (i != j)
+          @arestas << [grupo[i],grupo[j]]
+        end
+      end
+    end
+  end
+
 end
